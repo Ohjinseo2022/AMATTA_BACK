a001af5f525dbd5d787e6e5e0713f80fba886c4c
김성윤 <kimsy311@bnosoft.co.kr>
2024-12-07 17:59:34 +0900

    암호화 테스트 코드 추가

---
+++ src/test/java/com/amatta/config/JasyptConfigTest.java
@@ -0,0 +1,39 @@
+package com.amatta.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Configuration
+@EnableEncryptableProperties
+class JasyptConfigTest {
+
+    @Test
+    public void encryptDecryptTest() {
+        String text = "amatta";//변경할 값
+
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+
+        encryptor.setPassword("jasypt_key");
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+
+        String encryptedText = encryptor.encrypt(text);
+        System.out.println("enc : " + encryptedText);
+
+        String decryptedText = encryptor.decrypt(encryptedText);
+        System.out.println("dec : " + decryptedText);
+
+        assertEquals(text, decryptedText);
+
+        List.of("")
+            .stream()
+            .forEach(s -> {
+                System.out.println(s + " -> ENC(" + encryptor.encrypt(s)+")");
+            });
+    }
+}
